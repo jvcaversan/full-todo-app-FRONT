8b864ccbe5943dddb36d4db57dd71c23db03dc05
Joao Vitor Caversan <joaovitorcaversan@gmail.com>
2024-03-12 03:54:14 -0300

    feat: aplicando resyle e começando o front-end do blossom-app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from "@shopify/restyle";
+import Button from "components/button";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import theme, { Text } from "utils/theme";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text variant="textXl">Abre seu aplicativo agora</Text>
+        <Button />
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/components/button.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+import { Box, Text } from "utils/theme";
+
+const Button = () => {
+  return (
+    <Box bg="blu200" p="4" borderRadius="rounded-3xl">
+      <Text>Button or Components Update</Text>
+    </Box>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({});
